a8d980f48f1a9581999e2a4cda47e24f6b9ee525
Christopher Grace <chris@happylife.us>
2017-06-25 20:26:09 -0600

    Initial project add

---
+++ EF6JoinExperiment/School.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6JoinExperiment
+{
+    public static class School 
+    {
+        public static string GetStudentSeatLabel()
+        {
+            var context = new Ef6JoinExperimentEntities();
+            context.Database.Log = s => Debug.WriteLine(s);
+
+            var result = context.Students
+                .Where(s => s.FirstName == "Joe" && s.LastName == "Smith")
+                .Select( s=>s.Seat.SeatLabel).First();
+
+            return result;
+        }
+        public static List<string> GetStudentTutors()
+        {
+            var context = new Ef6JoinExperimentEntities();
+            context.Database.Log = s => Debug.WriteLine(s);
+            var result = context.Students
+                .Where(s => s.TutorId != null)
+                .Select(s => string.Concat(s.FirstName, " is tutored by ", s.Tutor.FirstName))
+                .ToList();
+
+            return result;
+        }
+        public static List<string> GetStudentsWithNationallyCertifiedTutor()
+        {
+            var context = new Ef6JoinExperimentEntities();
+            context.Database.Log = s => Debug.WriteLine(s);
+
+            var result = context.Students
+                .Where(s => s.Tutor.AssociationMemberships
+                    .Any(am=>am.TutorAssociation.Name 
+                        == "National Tutor Association"))
+                .Select(s => s.FirstName)
+                .ToList();
+            
+            return result;
+        }
+
+        public static List<string> GetClassroomSeats()
+        {
+            var context = new Ef6JoinExperimentEntities();
+            context.Database.Log = s => Debug.WriteLine(s);
+
+            var result = context.Classrooms
+                .FirstOrDefault(c => c.RoomNumber == 100)
+                .ClassroomSeats
+                .Select(cs => cs.Seat.SeatLabel)
+                .ToList();
+
+            result = context.ClassroomSeats
+                .Where(cs=>cs.Classroom.RoomNumber == 100)
+                .Select(cs => cs.Seat.SeatLabel)
+                .ToList();
+            return result;
+        }
+    }
+}
---
+++ EF6JoinExperimentTests/SchoolTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EF6JoinExperiment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6JoinExperiment.Tests
+{
+    [TestClass()]
+    public class SchoolTests
+    {
+        [TestMethod()]
+        public void GetStudentSeatLabelTest()
+        {
+            var result = School.GetStudentSeatLabel();
+            Assert.AreEqual("1", result);
+        }
+
+        [TestMethod()]
+        public void GetStudentTutorsTest()
+        {
+            var result = School.GetStudentTutors();
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod()]
+        public void GetStudentsWithNationallyCertifiedTutorTest()
+        {
+            var result = School.GetStudentsWithNationallyCertifiedTutor();
+            Assert.AreEqual("Jane", result);
+        }
+
+        [TestMethod()]
+        public void GetClassroomSeatsTest()
+        {
+            var result = School.GetClassroomSeats();
+            Assert.AreEqual(1, result.Count());
+        }
+    }
+}
